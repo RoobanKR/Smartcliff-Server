c28117996ec584a9129d72296e211899875b1d23
Rooban K. <1367@smartcliff.loc>
2025-03-25 17:04:42 +0530

    change domain name

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.use(
     origin: [
       // "http://localhost:3000",
       "http://smartcliff.academy",
+      "https://smart-cliff-next-js-git-develop-roobankrs-projects.vercel.app/"
       // "http://localhost:3535",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
